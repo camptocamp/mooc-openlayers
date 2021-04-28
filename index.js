089d111a63161bd02983656285bef68f6ff6b7fb
Marc Fasel <marc.fasel@camptocamp.com>
2021-04-28 10:28:48 +0200

    Update script

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 import 'ol/ol.css';
 import {Map, View} from 'ol';
 import TileLayer from 'ol/layer/Tile';
-import OSM from 'ol/source/OSM';
+import XYZ from 'ol/source/XYZ';
 
 const map = new Map({
   target: 'map',
   layers: [
     new TileLayer({
-      source: new OSM()
+      source: new XYZ({
+        attributions: ['Powered by Esri', 'Source: Esri, DigitalGlobe, GeoEye, Earthstar Geographics, CNES/Airbus DS, USDA, USGS, AeroGRID, IGN, and the GIS User Community'],
+        attributionsCollapsible: true,
+        url: 'https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+        maxZoom: 20
+      })
     })
   ],
   view: new View({
