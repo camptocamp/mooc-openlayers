c4f0e617a743d1d7299f760305ed97195f5fcd3b
Marc Fasel <marc.fasel@camptocamp.com>
2021-05-12 13:50:36 +0200

    update script

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ import {Map, View} from 'ol';
 import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
 import {OverviewMap, FullScreen, defaults as defaultControls} from 'ol/control';
-import {DragRotateAndZoom, defaults as defaultInteractions} from 'ol/interaction';
 
 var overviewMapControl = new OverviewMap({
   layers: [
@@ -12,29 +11,18 @@ var overviewMapControl = new OverviewMap({
     }) ],
 });
 
-var attributions =
-  '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> ' +
-  '<a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>';
-
 const map = new Map({
   target: 'map',
   layers: [
     new TileLayer({
-      source: new OSM(),
-      attributions: attributions
+      source: new OSM()
     })
   ],
   view: new View({
     center: [0, 0],
     zoom: 0,
-    minZoom: 2,
-    maxZoom: 17,
-    rotation: 0,
   }),
   controls: defaultControls().extend([
-    overviewMapControl,
-    new FullScreen({
-      source: 'fullscreen',
-    })
+    overviewMapControl
   ])
 })
