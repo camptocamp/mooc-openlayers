df414dbd2b8649f113fb59eddd6031b9e2e94289
Marc Fasel <marc.fasel@camptocamp.com>
2021-05-12 14:31:45 +0200

    Update basemap layer

--- index.js
+++ index.js
@@ -3,17 +3,19 @@ import {Map, View} from 'ol';
 import TileLayer from 'ol/layer/Tile';
 import XYZ from 'ol/source/XYZ';
 
+let basemapLayer = new TileLayer({
+  source: new XYZ({
+    attributions: ['Powered by Esri', 'Source: Esri, DigitalGlobe, GeoEye, Earthstar Geographics, CNES/Airbus DS, USDA, USGS, AeroGRID, IGN, and the GIS User Community'],
+    attributionsCollapsible: true,
+    url: 'https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+    maxZoom: 20
+  })
+})
+
 const map = new Map({
   target: 'map',
   layers: [
-    new TileLayer({
-      source: new XYZ({
-        attributions: ['Powered by Esri', 'Source: Esri, DigitalGlobe, GeoEye, Earthstar Geographics, CNES/Airbus DS, USDA, USGS, AeroGRID, IGN, and the GIS User Community'],
-        attributionsCollapsible: true,
-        url: 'https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
-        maxZoom: 20
-      })
-    })
+    basemapLayer
   ],
   view: new View({
     center: [0, 0],
