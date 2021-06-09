00079e7594a61c7a157b5820d81d1de5590c52f2
Marc Fasel <marc.fasel@camptocamp.com>
2021-06-09 13:08:08 +0200

    Collapse attribution

--- index.js
+++ index.js
@@ -124,7 +124,7 @@ const map = new Map({
     center: [0, 0],
     zoom: 0,
   }),
-  controls: defaultControls().extend([
+  controls: defaultControls({ attributionOptions: { collapsible: true }}).extend([
     overviewMapControl,
     zoomToExtentControl,
     scaleControl()
